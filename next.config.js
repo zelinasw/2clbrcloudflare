6d25659301c967d97fb792cc9c68ae1426e57506
zelinasw <zelinaaswa@gmail.com>
2026-05-07 00:20:22 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Versi Next.js 15 lebih suka runtime didefinisikan di level halaman, 
-  // tapi kalau mau global di sini saja:
-  experimental: {
-    // Kosongkan saja kalau tidak yakin, Next.js 15 sudah pintar
-  }
+  // Biarkan kosong atau standar saja
 };
 
 module.exports = nextConfig;
